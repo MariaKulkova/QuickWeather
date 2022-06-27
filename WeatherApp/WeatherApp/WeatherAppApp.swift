5b6f6fad6d5a5f3d10e9452a3a68b3c5d40ae0f2
Maria Kulkova <maria@trala.com>
2022-06-27 22:07:39 +0300

    Initial project structure

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Maru on 27.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
